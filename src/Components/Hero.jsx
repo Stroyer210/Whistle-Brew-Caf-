168e157bea240f778efc0c263b85a284e6ddd502
Stroyer210 <sergiosthiwar@gmail.com>
2024-07-10 12:37:48 -0500

    intalled tailwind plus created a couple files and folders

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,16 @@
+import "./../App.css";
+import { Link } from "react-router-dom";
+// import Footer from './Footer';
+// import Navbar from './Navbar';
+
+
+const Hero = () => {
+    return (
+        <div className="">
+            <h1 className="text-center">hello</h1>
+            {/* <Footer /> */}
+        </div>
+    )
+};
+
+export default Hero;
