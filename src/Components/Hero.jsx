c7a40f34b4bdf00c241dad299a612066f4cf202c
Stroyer210 <sergiosthiwar@gmail.com>
2024-07-10 12:53:56 -0500

    adding a couple grids to the project and images

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,13 +1,22 @@
 import "./../App.css";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 // import Footer from './Footer';
 // import Navbar from './Navbar';
-
+import barista from "./../Images/hero.avif"
+import main from "./../Images/main.avif"
 
 const Hero = () => {
     return (
         <div className="">
-            <h1 className="text-center">hello</h1>
+            <img src={main} alt="pic of the place" className="brightness-50"/>
+            <div className="grid grid-cols-2 ">
+                <div className="">
+                    <img src={barista} alt="" />
+                </div>
+                <div>
+                    <h1 className="text-3xl text-gray-800 text-center  my-auto">Welcome to the best cafe</h1>
+                </div>
+            </div>
             {/* <Footer /> */}
         </div>
     )
