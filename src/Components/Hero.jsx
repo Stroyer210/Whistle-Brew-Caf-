0112f3f5118f556a1520004c85f7a7c45835bdc7
Stroyer210 <sergiosthiwar@gmail.com>
2024-07-15 12:21:56 -0500

    fixing a bug

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -10,13 +10,19 @@ const Hero = () => {
     return (
         <div className="">
             <Navbar className=""/>
-            <img src={main} alt="pic of the place" className="brightness-50 w-full object-cover"/>
+            <img src={main} alt="pic of the place" className="brightness-50 w-full h-screen object-cover"/>
             <div className="grid grid-cols-2 ">
                 <div className="">
-                    <img src={barista} alt="barista" className="w-full h-96 object-cover"/>
+                    <img src={barista} alt="barista" className="w-full h-full object-cover"/>
                 </div>
-                <div>
-                    <h1 className="text-3xl text-gray-800 text-center  my-auto sen">Welcome to the best cafe</h1>
+                <div className="text-center 2xl:my-10">
+                    <h1 className="text-3xl text-gray-800 text-center my-auto sen font-bold mt-10 2xl:mt-14">THE LATEST</h1>
+                    <hr className="mx-28 xl:mx-60 rounded border-2 border-orange-300 md:mb-5 2xl:mb-14 xl:mt-2"/>
+                    <h1 className="text-xl text-gray-800 text-center 2xl:ml-20 my-auto sen"> CHECK OUR MENU • </h1>
+                    <button className="py-3 px-5 sen bg-orange-300 xl:text-xl md:mt-5">Menu</button>
+                    <h1 className="text-xl text-gray-800 text-left md:ml-10 xl:ml-20 my-auto sen mt-10">COFFEE + ESPRESSO CATERING • </h1>
+                    <h1 className="text-xl text-gray-800 text-left md:ml-10 xl:ml-20 my-auto sen">Now booking 2024!</h1>
+                    <h1 className="text-xl text-orange-400 text-center  my-auto sen md:mt-2 xl:mt-8 2xl:mb-12 underline">Learn More</h1>
                 </div>
             </div>
             <div className="grid grid-cols-2 ">
@@ -28,7 +34,7 @@ const Hero = () => {
                     <h1 className="text-xl text-gray-800 text-left md:ml-10 xl:ml-20 my-auto sen">MON-SAT 7am - 8pm</h1>
                     <h1 className="text-xl text-gray-800 text-left md:ml-10 xl:ml-20 my-auto sen">SUNDAY 7am - 6pm</h1>
                     <h1 className="text-xl text-gray-800 text-left md:ml-10 xl:ml-20 my-auto sen md:mt-2 xl:mt-8 xl:mb-12">Full coffee + espresso menu.</h1>
-                    <button className="py-3 px-5 sen bg-orange-300 xl:text-xl">GET DIRECTIONS</button>
+                    <button className="py-3 px-5 sen bg-orange-300 xl:text-xl md:my-3">GET DIRECTIONS</button>
                 </div>
                 <div className="">
                     <img src={coffee} alt="coffee" className="w-full h-full object-cover"/>
