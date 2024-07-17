0d0a49d9a13507950216e9b74f20b3b0b1abf7fb
Stroyer210 <sergiosthiwar@gmail.com>
2024-07-17 12:57:32 -0500

    making the navbar responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,22 +1,39 @@
 // import React from "react";
 import "./../App.css";
 import { Link } from "react-router-dom";
+import {FaBars, FaTimes} from "react-icons/fa";
+import { useRef } from "react";
 
 const Navbar = () => {
+    const navRef = useRef();
+
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+        document.body.classList.toggle("overflow-hidden");
+    }
+
     return (
-        <div className="flex justify-between mx-10 2xl:mx-96 mt-10 p-5 text-4xl font-bold bg-transparent absolute w-full z-50">
-            <div className="">
-                <h1 className=" text-gray-200 text-shadow sen">Whistle & Brew Café</h1>
-            </div>
-            <div className="text-white text-shadow sen ">
-                <Link to="/" className="inline mr-20 cursor-pointer">Main</Link>
-                <Link to="/" className="inline mr-20 cursor-pointer">Coffee</Link>
-                <Link to="/" className="inline mr-20 cursor-pointer">Expresso</Link>
-                <Link to="/" className="inline mr-20 cursor-pointer">Tea</Link>
-                <Link to="/" className="inline mr-20 cursor-pointer">Bakery</Link>
+        <header className=" mt-10 p-5 text-4xl font-bold bg-transparent absolute w-full z-50">
+            <div className="flex justify-between items-center">
+                <div className="ml-40">
+                    <h1 className=" text-gray-200 text-shadow sen lg:mr-40 xl:mr-40 2xl:mr-72">Whistle & Brew Café</h1>
+                </div>
+                <nav ref={navRef} className="text-white text-shadow sen">
+                    <Link to="/" className=" mr-10 lg:mr-20 cursor-pointer hover:text-yellow-600">Main</Link>
+                    <Link to="/" className=" mr-10 lg:mr-20 cursor-pointer hover:text-yellow-600">Coffee</Link>
+                    <Link to="/" className=" mr-10 lg:mr-20 cursor-pointer hover:text-yellow-600">Expresso</Link>
+                    <Link to="/" className=" mr-10 lg:mr-20 cursor-pointer hover:text-yellow-600">Tea</Link>
+                    <Link to="/" className=" mr-10 lg:mr-20 cursor-pointer hover:text-yellow-600">Bakery</Link>
+                    <button onClick={showNavbar} className="nav-btn nav-close-btn">
+                        <FaTimes/>
+                    </button>
+                </nav>
+                <button onClick={showNavbar} className="nav-btn m-0 inline mr-10">
+                <FaBars className="text-white hover:text-yellow-600"/>
+                </button>
             </div>
 
-        </div> 
+        </header> 
     )
 };
 
