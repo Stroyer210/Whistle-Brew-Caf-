b98b781257df73933a4240c39f2a2bf089f63d50
Stroyer210 <sergiosthiwar@gmail.com>
2024-07-18 13:42:46 -0500

    adding the routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import './custom.css';
 import Hero from './Components/Hero';
+import Coffee from './Components/Coffee';
+import Expresso from './Components/Expresso';
+import Tea from './Components/Tea';
+import Bakery from './Components/Bakery';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -8,6 +12,10 @@ function App() {
     <Router>
       <Routes>
         <Route exact path="/" element={<Hero />} />
+        <Route path="/Coffee" element={<Coffee />} />
+        <Route path="/Expresso" element={<Expresso />} />
+        <Route path="/Tea" element={<Tea />} />
+        <Route path="/Bakery" element={<Bakery />} />
       </Routes>
     </Router>
   );
