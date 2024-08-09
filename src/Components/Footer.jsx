11a0637933e6fcc50243364d9c3400b12586c94c
Stroyer210 <sergiosthiwar@gmail.com>
2024-08-09 12:40:06 -0500

    adding hover styles to the footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,4 +1,5 @@
 import "./../App.css";
+import "./../custom.css"
 import React from "react";
 import {
   AiOutlineInstagram,
@@ -11,26 +12,26 @@ const Footer = () => {
     return (
         <div className="flex justify-between p-5 text-lg font-bold bg-gray-800">
         <div className="max-w-[1200px] sm:h-[150px] p-4 sm:p-12 lg:flex justify-between mx-auto items-center text-center">
-          <span className="text-gray-300 dark:text-gray-600 biorhyme">
+          <span className="text-gray-300 dark:text-gray-600 biorhyme contact-shadow">
             Whistle & Brew Café
           </span>
           
           <div className="text-4xl flex flex-wrap justify-center sm:mx-10 lg:mx-28 gap-6 sm:gap-16 py-3 text-gray-300 dark:text-white mt-3 sm:mt-0">
-            <a href="https://www.instagram.com/sthiwar_17/" target="_blank" rel="noreferrer">
-              <AiOutlineInstagram className="contact-shadow dark:text-gray-100 dark:hover:text-gray-900" />
+            <a href="https://www.instagram.com/sthiwar_17/" target="_blank" rel="noreferrer" className="contact-shadow">
+              <AiOutlineInstagram className="contact-shadow hover:text-orange-300" />
             </a>
             <a href="https://www.linkedin.com/in/sergio-ardila-alvarado-550b40152/" target="_blank" rel="noreferrer">
-              <AiFillLinkedin className="contact-shadow dark:text-gray-100 dark:hover:text-gray-900" />
+              <AiFillLinkedin className="contact-shadow hover:text-orange-300" />
             </a>
             <a href="https://github.com/Stroyer210/WhistleBrew-Cafe" target="_blank" rel="noreferrer">
-              <AiFillGithub className="contact-shadow dark:text-gray-100 dark:hover:text-gray-900" />
+              <AiFillGithub className="contact-shadow hover:text-orange-300" />
             </a>
             <a href="https://www.facebook.com/sthiwar13" target="_blank" rel="noreferrer">
-              <AiFillFacebook className="contact-shadow dark:text-gray-100 dark:hover:text-gray-900" />
+              <AiFillFacebook className="contact-shadow hover:text-orange-300" />
             </a>
           </div>
       
-          <p className="text-gray-300 mt-3 sm:mt-0 text-sm">
+          <p className="text-gray-300 mt-3 sm:mt-0 text-sm contact-shadow">
             ©2024 Whistle & Brew Café <br /> All rights reserved
           </p>
         </div>
